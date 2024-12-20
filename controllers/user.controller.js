0a0d13df1a7a183c7690a2d3517693c967e1f798
Deepeshnama <deepeshnama0786@gmail.com>
2024-12-20 09:34:39 +0530

    added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -77,4 +77,14 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find(); 
+    res.status(200).json(users); 
+  } catch (error) {
+    res.status(500).json({ message: error.message }); 
+  }
+};
+
+export { register, login , getAllUsers };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { getAllUsers, login, register } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+userRouter.get("/" , getAllUsers)
 
 export default userRouter;
